6698edb7a7d86ac6e28f2a4edfef7361b077fc27
Klodian Doka <155301614+klodi2203@users.noreply.github.com>
2026-07-18 13:07:01 +0300

    Add: is-anagram - submission-2

---
+++ Data Structures & Algorithms/is-anagram/submission-2.java
@@ -0,0 +1,25 @@
+class Solution {
+    public boolean isAnagram(String s, String t) {
+
+        if(s.length()!= t.length()){
+            return false;
+        }
+
+        int[] charArray = new int[26];
+
+        for(int i = 0; i < s.length(); i++){
+            charArray[s.charAt(i) - 'a']++;
+            charArray[t.charAt(i) - 'a']--;
+        }
+
+
+        for(int c : charArray){
+            if(c != 0){
+                return false;
+            }
+        }
+
+        return true;
+
+    }
+}
